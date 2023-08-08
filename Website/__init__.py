b2915efa76241325b4cee56d75c6e7fefda8dcad
Pranaya <pranaya@Pranayas-MacBook-Air.local>
2023-08-08 22:11:12 +0200

    some code design editing

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,14 +1,14 @@
-#Setting up flask application
+# Setting up flask application
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Mamaissopretty27081966'
 
-    #Registering Blueprints to Flask
+    # Registering Blueprints to Flask
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix = '/')
+    app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     return app

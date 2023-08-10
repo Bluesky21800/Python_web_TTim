e625b24cbf9021bdd61602108678f7478f3aaf50
Pranaya <pranaya@Pranayas-MacBook-Air.local>
2023-08-10 22:05:20 +0200

    rendering HTML Template (Small Debug, HTML Writing at the top somehow :"D

--- Website/views.py
+++ Website/views.py
@@ -1,11 +1,10 @@
 #making blueprints with roots / urls inside for everything view related
-from flask import Blueprint
+from flask import Blueprint, render_template
+
 
 views = Blueprint('views', __name__)
 
 #decorator for route for main page
 @views.route('/')
 def home():
-    return "<h1>test</h1>"
-
-#i love pizza
+    return render_template("home.html")
